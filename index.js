17bddfe19c11566a83801dd0969c48c852d5ee23
edmilsonsgjk <edmilsonsgjk@gmail.com>
2019-08-28 20:25:49 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+let net;
+
+async function app() {
+  console.log('Loading mobilenet..');
+
+  // Load the model.
+  net = await mobilenet.load();
+  console.log('Sucessfully loaded model');
+
+  // Make a prediction through the model on our image.
+  const imgEl = document.getElementById('img');
+  const result = await net.classify(imgEl);
+  console.log(result);
+    
+  // Show predction and probability.
+  
+  document.getElementById('console').innerText = `
+      Prediction: ${result[0].className}\n
+      Probability: ${result[0].probability}
+    `;
+}
+
+app();
